887ed9b6d8b9c80c83a6aee14c73bb4eb9bcb53c
Roman <MrFiskerton@users.noreply.github.com>
2016-06-25 11:29:55 +0300

    Update lazy_string.cpp

--- task6/lazy_string.cpp
+++ task6/lazy_string.cpp
@@ -4,13 +4,13 @@ using namespace std;
 
 lazy_string::lazy_string() {
     (*this).begin = (*this).end = 0;
-    (*this).data = make_shared<string>("");
+    this->data = make_shared<string>("");
 }
 
 lazy_string::lazy_string(const string &str) {
     (*this).begin = 0;
     (*this).end = str.size();
-    this->data = make_shared<string>(str);
+    (*this).data = make_shared<string>(str);
 }
 
 lazy_string::lazy_string(size_t begin, size_t size, shared_ptr<string> str) {
@@ -21,7 +21,7 @@ lazy_string::lazy_string(size_t begin, size_t size, shared_ptr<string> str) {
 
 size_t lazy_string::size() const { return end; }
 
-size_t lazy_string::length() const { return end; }
+size_t lazy_string::length() const {  return end; }
 
 istream &operator>>(istream &input, lazy_string &str) {
     auto s = make_shared<string>();
@@ -37,6 +37,8 @@ ostream &operator<<(ostream &output, lazy_string &str) {
     return output;
 }
  
+
+
 lazy_string lazy_string::substr(size_t pos, size_t length) {
     return lazy_string(begin + pos, (pos + length > end) ? (end - pos) : length, data);
 }
