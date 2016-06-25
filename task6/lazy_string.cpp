a0f9571449024e832c5e61178ea2eda4a7b8b508
Roman <MrFiskerton@users.noreply.github.com>
2016-06-25 11:47:31 +0300

    Update lazy_string.cpp

--- task6/lazy_string.cpp
+++ task6/lazy_string.cpp
@@ -1,44 +1,79 @@
-#pragma once
+#include "lazy_string.h"
 
-#include <string>
-#include <iostream>
-#include <memory>
+using namespace std;
 
-class lazy_string {
-    struct get_char {
-    public:
-        friend class lazy_string;
-        operator char() const;
-        get_char &operator=(char);
-    private:
-        get_char(lazy_string *, size_t);
-        const size_t index;
-        lazy_string *const lazy_str;
-    };
+lazy_string::lazy_string() {
+    (*this).begin = (*this).end = 0;
+    this->data = make_shared<string>("");
+}
 
-private:
-    size_t begin, end;
-    std::shared_ptr<std::string> data;
-    lazy_string(size_t begin, size_t size, std::shared_ptr<string> str);
+lazy_string::lazy_string(const string &str) {
+    (*this).begin = 0;
+    (*this).end = str.size();
+    (*this).data = make_shared<string>(str);
+}
 
-public:
-    lazy_string();                  //Create empty lazy string
-    lazy_string(const std::string &str); //Create lazy string from std::string
-    operator std::string();         //Create string copy from lazy string
-    size_t size() const;            //Return length of lazy sting
-    size_t length() const;          //Return length of lazy sting
+lazy_string::lazy_string(size_t begin, size_t size, shared_ptr<string> str) {
+    (*this).begin = begin;
+    (*this).end = size;
+    (*this).data = str;
+}
 
-    //Return the substring from position begin to begin + length (or to the end of a lazy string)
-    lazy_string substr(size_t begin, size_t length = std::string::npos);
-    //Read lazy string from input stream
-    friend std::istream &operator>>(std::istream &input, lazy_string &str);
-    //Sends lazy string to output stream
-    friend std::ostream &operator<<(std::ostream &output, lazy_string &str);
+size_t lazy_string::size() const { return end; }
 
-    get_char at(size_t i);          //Return the character of lazy string
-    char at(size_t i) const;
-    get_char operator[](size_t i);
-    char operator[](size_t i) const;
+size_t lazy_string::length() const {  return end; }
 
-};
+istream &operator>>(istream &input, lazy_string &str) {
+    auto s = make_shared<string>();
+    input >> *s;
+    str.data = s;
+    str.begin = 0;
+    str.end = (*s).size();
+    return input;
+}
 
+ostream &operator<<(ostream &output, lazy_string &str) {
+    for (size_t i = str.begin; i < str.begin + str.size(); i++) { output << str[i]; }
+    return output;
+}
+ 
+
+
+lazy_string lazy_string::substr(size_t pos, size_t length) {
+    return lazy_string(begin + pos, (pos + length > end) ? (end - pos) : length, data);
+}
+
+lazy_string::operator std::string() {
+    return (*data).substr(begin, end);
+}
+
+char lazy_string::at(size_t i) const {
+    return (*data)[begin + i];
+}
+
+lazy_string::get_char lazy_string::at(size_t i) {
+    return get_char(this, i);
+}
+
+char lazy_string::operator[](size_t i) const {
+    return (*data)[begin + i];
+}
+
+lazy_string::get_char lazy_string::operator[](size_t i) {
+    return get_char(this, i);
+}
+
+lazy_string::get_char::get_char(lazy_string *str, size_t i) : lazy_str(str), index(i) { }
+
+lazy_string::get_char &lazy_string::get_char::operator=(char c) {
+    if ((*lazy_str).data.use_count() > 1) {
+        (*lazy_str).begin = 0;
+        (*lazy_str).data = make_shared<string>(lazy_str->data->substr((*lazy_str).begin, (*lazy_str).end));
+    }
+    (*lazy_str->data)[(*lazy_str).begin + index] = c;
+    return *this;
+}
+
+lazy_string::get_char::operator char() const {
+    return (*lazy_str->data)[lazy_str->begin + index];
+}
